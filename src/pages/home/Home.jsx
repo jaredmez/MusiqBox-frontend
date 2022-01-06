49b47e31364d8eb941baa050805487bb0d410ebb
Jared M <mezquitaj@gmail.com>
2022-01-06 15:18:34 -0500

    add some styling to main pages

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="home">
       <h2>Home Page</h2>
-      <div>content coming soon</div>
+      <div>Welcome!</div>
     </div>
   )
 }
